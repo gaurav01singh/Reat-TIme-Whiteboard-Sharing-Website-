51e2513bfbc176f17110bc08e399ca338f73e48a
Gaurav Kumar Singh <gauravsingh02195@gmail.com>
2024-07-23 18:51:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const { userJoin, getUsers, userLeave } = require("./utils/user");
+const socketIO = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const socketIO = require("socket.io");
 const io = socketIO(server);
 
 app.use(cors({
@@ -13,6 +13,7 @@ app.use(cors({
   methods: ["POST", "GET"],
   credentials: true
 }));
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -64,9 +65,7 @@ io.on("connection", (socket) => {
   });
 });
 
-// serve on port
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () =>
-  console.log(`server is listening on http://localhost:${PORT}`)
-);
+// Export serverless function
+module.exports = (req, res) => {
+  server.emit('request', req, res);
+};
